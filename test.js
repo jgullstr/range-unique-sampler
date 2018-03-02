67627c35e71beedee4a9489b55719bf0c92efb39
Josef Gullström <josef@pool.fi>
2018-03-02 21:05:42 +0200

    Numeric sort of test sample.

--- test.js
+++ test.js
@@ -2,14 +2,14 @@ import test from 'ava';
 import uniqueSampler from './';
 
 test('main', t => {
-  const min = 100;
-  const max = 1000;
-  const length = max - min
+  const min = 0;
+  const max = 100;
+  const length = max - min;
   const sampler = uniqueSampler(min, max);
   const all_values = Array.from({length: length}, (v, i) => i + min);
   const all_samples = Array.from({length: length}, sampler);
 
   t.notDeepEqual(all_values, all_samples);
-  t.deepEqual(all_values, all_samples.sort());
+  t.deepEqual(all_values, all_samples.sort((a,b) => a - b));
   t.throws(sampler);
 })
